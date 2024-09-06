4fc0dc4f90b4dc958b16dd4fe7301f474b5f3595
AValleO <andres.valle@bayteq.com>
2024-09-06 03:56:28 -0500

    Init App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'compressor-app';
+  photo: string | undefined;
+  photoFilename: string | undefined;
+  photoSize: number | undefined;
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl
+    });    
+    console.log(image);
+    this.photo = image.dataUrl;
+    this.photoSize = image.dataUrl ? Math.round((image.dataUrl.length * (3/4)) / 1024) : 0;
+  }
 }
